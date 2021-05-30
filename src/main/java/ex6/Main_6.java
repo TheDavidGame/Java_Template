212d84a50ba43af4d96393afc08bfe4ab91b268f
TheDavidGame <vlad.billI@bk.ru>
2021-05-31 02:00:13 +0300

    sec

--- src/main/java/ex6/Main_6.java
+++ src/main/java/ex6/Main_6.java
@@ -1,10 +1,14 @@
 package ex6;
 
 import ex6.abstract_fabrik.Client;
+import ex6.ptototype.Shape;
+import ex6.ptototype.triangle;
 
 public class Main_6 {
     public static void main(String[] args) {
+
         Client client = new Client();
         client.clear();
+
     }
 }
